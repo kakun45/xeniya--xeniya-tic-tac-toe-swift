9351315a4786cad6d6997a03b13a0be4d9ab7b4a
kakun45 <email4blackkat@gmail.com>
2024-10-17 16:11:44 -0400

    enum

--- xeniya- xeniya-tic-tac-toe-swift/Move.swift
+++ xeniya- xeniya-tic-tac-toe-swift/Move.swift
@@ -8,12 +8,26 @@
 import Foundation
 
 // one of 3 ways to make a type
-enum Sign() {
-    
+enum Sign {
+    case rock
+    case paper
+    case scissors
 }
 
 // model the structure: keepss track of 3 possible moves in the game
+// emoji hot keys ctr+cmd+space
 struct Move {
     var sign: Sign
-    var emoji: String
+//    computed property one of enum choices
+    var emoji: String {
+        switch sign {
+        case .rock:
+            return "👊"
+        case .paper:
+            return "✋"
+        case .scissors:
+            return "✌️"
+        }
+    }
+    
 }
