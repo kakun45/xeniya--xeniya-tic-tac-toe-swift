e6067220b5fc731c4382d02046fe9e2c79e088b2
kakun45 <email4blackkat@gmail.com>
2024-10-17 17:00:21 -0400

    logic of wins

--- xeniya- xeniya-tic-tac-toe-swift/Move.swift
+++ xeniya- xeniya-tic-tac-toe-swift/Move.swift
@@ -15,10 +15,10 @@ enum Sign {
 }
 
 // model the structure: keepss track of 3 possible moves in the game
-// emoji hot keys ctr+cmd+space
+// emoji hot keys ctr+cmd+space with referee() method to keep track of the GameState based on opponent's choice
 struct Move {
     var sign: Sign
-//    computed property one of enum choices
+    // computed property one of enum choices
     var emoji: String {
         switch sign {
         case .rock:
@@ -29,5 +29,22 @@ struct Move {
             return "✌️"
         }
     }
-    
+    func referee(against opponent: Move) -> GameState {
+        //        tie
+        if self.sign == opponent.sign {
+            return .draw
+        }
+        
+        //        win
+        switch self.sign {
+        case .rock:
+            if opponent.sign == .scissors {return .win}
+        case .paper:
+            if opponent.sign == .rock {return .win}
+        case .scissors:
+            if opponent.sign == .paper {return .win}
+        }
+        //        lose
+        return .lose
+    }
 }
