1763cfe94e497839adb9248f3cba6afac54be63a
kakun45 <email4blackkat@gmail.com>
2024-10-17 15:56:18 -0400

    WIP

---
+++ xeniya- xeniya-tic-tac-toe-swift/Move.swift
@@ -0,0 +1,19 @@
+//
+//  Move.swift
+//  xeniya- xeniya-tic-tac-toe-swift
+//
+//  Created by Guest User on 10/17/24.
+//
+
+import Foundation
+
+// one of 3 ways to make a type
+enum Sign() {
+    
+}
+
+// model the structure: keepss track of 3 possible moves in the game
+struct Move {
+    var sign: Sign
+    var emoji: String
+}
